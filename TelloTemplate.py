f29cd9d2aace0ccd1a7338601c7e5e98c1d8288f
Liam Treat <Treatliam92@gmail.com>
2026-04-13 13:11:44 -0400

    forward 100 cm Workinhg

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -59,8 +59,7 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Review the (SDK) Software Development Kit resource for Drone Commands
-        # Delete these comments before writing your program
+        sendmsg ('forward 100')
 
         sendmsg('land')
 
